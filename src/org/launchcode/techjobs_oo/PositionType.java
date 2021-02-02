1a2c688c6d8bf85e23f7d0cf0982a945135d2842
Diana Che <dianache@Dianas-MacBook-Pro.local>
2021-02-02 08:04:49 -0600

    Completed Step 3: the job class

--- src/org/launchcode/techjobs_oo/PositionType.java
+++ src/org/launchcode/techjobs_oo/PositionType.java
@@ -23,34 +23,20 @@ public class PositionType {
     public String toString() { return value;}
 
 
-// TODO: Add custom equals and hashCode methods. Consider two PositionType objects "equal" when
-    //  their id fields match.
+// TODO: Add custom equals and hashCode methods. Consider two PositionType objects "equal" when their id fields match.
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         PositionType that = (PositionType) o;
-        return id == that.id; //&& value.equals(that.value);
+        return id == that.id;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, value);
+        return Objects.hash(id);
     }
-
-
-//
-//    @Override
-//    public boolean equals(Object o) {
-//        if (this == o) return true;
-//        if (!(o instanceof Location)) return false;
-//        Location location = (Location) o;
-//        return getId() == location.getId();
-//    }
-//
-
-
 
 
     // Getters and Setters:
---
+++ src/org/launchcode/techjobs_oo/Tests/JobTest.java
@@ -0,0 +1,20 @@
+//package org.launchcode.techjobs_oo.Tests;
+//
+//import org.launchcode.techjobs_oo.Job;
+//
+//public class JobTest {
+//
+//    Job test_job;
+//
+//    @Before
+//    public void createJobObject() {
+//        test_job = new Job("Toyota", "Prius", 10, 50);
+//    }
+//
+//
+//
+//    @Test
+//    public void testSettingJobId() {
+//
+//    }
+//}
